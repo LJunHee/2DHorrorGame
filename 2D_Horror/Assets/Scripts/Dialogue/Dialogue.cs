2433fb572a46c1111c5cf79544089a25b10df93d
LJunHee <100752571+LJunHee@users.noreply.github.com>
2024-05-03 16:10:08 +0900

    대화
    대화창

--- 2D_Horror/Assets/Scripts/Dialogue/Dialogue.cs
+++ 2D_Horror/Assets/Scripts/Dialogue/Dialogue.cs
@@ -10,6 +10,7 @@ public class Dialogue : MonoBehaviour
     public float textSpeed;
 
     private int index;
+    private bool isTyping;
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +22,7 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            if(textComponent.text == lines[index])
-            {
-                NextLine();
-            }
-            else
-            {
-                StopAllCoroutines();
-                textComponent.text = lines[index];
-            }
-        }
+
     }
 
     void StartDialogue()
@@ -43,24 +33,29 @@ public class Dialogue : MonoBehaviour
 
     IEnumerator TypeLine()
     {
+        isTyping = true;
         foreach (char c in lines[index].ToCharArray())
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+        isTyping = false;
     }
 
-    void NextLine()
+    public void NextLine()
     {
-        if (index < lines.Length - 1)
+        if (!isTyping)
         {
-            index++;
-            textComponent.text = string.Empty;
-            StartCoroutine (TypeLine());
-        }
-        else
-        {
-            gameObject.SetActive(false);
+            if (index < lines.Length - 1)
+            {
+                index++;
+                textComponent.text = string.Empty;
+                StartCoroutine(TypeLine());
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
         }
     }
 }
