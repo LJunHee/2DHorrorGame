5324b66104f5d1710052936858c628338d3bb29b
LJunHee <100752571+LJunHee@users.noreply.github.com>
2024-05-06 16:47:45 +0900

    전체적인 UI 수정
    05.06 수정

--- 2D_Horror/Assets/Scripts/Dialogue/Dialogue.cs
+++ 2D_Horror/Assets/Scripts/Dialogue/Dialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.EventSystems;
 
 public class Dialogue : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class Dialogue : MonoBehaviour
     public float textSpeed;
 
     private int index;
+    private bool isTyping; // 타이핑 중인지 여부를 나타내는 변수
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,11 @@ public class Dialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        // 대화창을 클릭하면 다음 대화로 넘어감
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            NextLine();
+        }
     }
 
     void StartDialogue()
@@ -32,27 +38,34 @@ public class Dialogue : MonoBehaviour
 
     IEnumerator TypeLine()
     {
+        isTyping = true; // 타이핑이 시작됨을 나타냄
+
         foreach (char c in lines[index].ToCharArray())
         {
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
         }
+
+        isTyping = false; // 타이핑이 끝났음을 나타냄
     }
 
     public void NextLine()
     {
         if (index < lines.Length - 1)
         {
-            if(index == )
+            if (isTyping)
+            {
+                // 타이핑 중에 클릭되면 타이핑을 멈추고 전체 텍스트를 표시
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+                isTyping = false; // 타이핑이 끝났음을 나타냄
+            }
+            else
             {
                 index++;
                 textComponent.text = string.Empty;
                 StartCoroutine(TypeLine());
-            }else
-            {
-
             }
-
         }
         else
         {
